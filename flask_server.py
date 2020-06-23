266f4e1c86fe8e4ad05bde257e7b40f87a1bda41
Weronika Żak <weronikarzak@gmail.com>
2020-06-24 00:43:30 +0200

    new model

--- flask_server.py
+++ flask_server.py
@@ -4,6 +4,7 @@ import cv2
 import json
 import numpy as np
 import io
+from tensorflow.keras.preprocessing import image
 
 
 app = Flask(__name__)
@@ -33,7 +34,10 @@ def preprocessing(file):
 	file.save(in_memory_file)
 	data = np.fromstring(in_memory_file.getvalue(), dtype=np.uint8)
 	img = cv2.imdecode(data)
-	img = 
+	img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = cv2.resize(img, (224, 224))
+    img1 = image.img_to_array(img) / 255
+    img1 = np.expand_dims(img1, axis=0)
 	return img
 
 
